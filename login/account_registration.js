b5d17b9dcce82d81f669923c81ef16c1bd859a7e
Kitayamashota <nkc20230101@st.denpa.jp>
2023-11-20 12:55:18 +0900

    (仮)コード変えた

---
+++ login/account_registration.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const registerBtn = document.getElementById("register-btn");
+
+    registerBtn.addEventListener("click", function () {
+        const emailInput = document.getElementById('login-email');
+        const usernameInput = document.getElementById('login-name');
+        const passwordInput = document.getElementById('login-pass');
+        const confirmPasswordInput = document.getElementById('confirm-pass');
+
+        const isAnyFieldEmpty = [usernameInput, emailInput, passwordInput, confirmPasswordInput].some(function (input) {
+            return input.value.trim() === '';
+        });
+
+        if (isAnyFieldEmpty) {
+            alert("いずれかの項目が入力されていません。");
+        } else {
+            alert("保存しました");
+        }
+    });
+});
