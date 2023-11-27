bed37aa65f17c64830c4397935e7eba2c4d7acbf
Hattori3333 <nkc20230144@st.denpa.jp>
2023-11-27 12:59:36 +0900

    ログイン機能の追加

--- php/login.php
+++ php/login.php
@@ -1,37 +1,44 @@
 <?php
-    $dsn = 'mysql:host=aichi-database-1.clra1wx84nvf.ap-south-1.rds.amazonaws.com;dbname=aichi_log_db;charset=UTF8mb4'; 
-    $user = 'admin';
-    $pass = 'password';
+$dsn = 'mysql:host=aichi-database-1.clra1wx84nvf.ap-south-1.rds.amazonaws.com;dbname=aichi_log_db;charset=UTF8mb4'; 
+$user = 'admin';
+$pass = 'password';
 
-    try {
-        $dbh = new PDO($dsn, $user, $pass, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        ]);
+try {
+    $dbh = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
 
-        $sql = 'SELECT * FROM user';
-        $stmt = $dbh->query($sql);
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // フォームから送信されたメールアドレスとパスワード
+        $user_input_email = isset($_POST['emailaddress']) ? $_POST['emailaddress'] : '';
+        $user_input_password = isset($_POST['password']) ? $_POST['password'] : '';
 
-        $dbh = null;
+        // ユーザー認証のためのSQLクエリ
+        $sql = 'SELECT * FROM user WHERE emailaddress = :email';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':email', $user_input_email, PDO::PARAM_STR);
+        $stmt->execute();
 
-        if (!empty($result)) {
-            echo '<table>';
-            echo '<tr><th>ID</th><th>ユーザー名</th><th>メールアドレス</th></tr>';
-            foreach ($result as $row) {
-                echo '<tr>';
-                echo '<td>' . $row['userid'] . '</td>';
-                echo '<td>' . $row['username'] . '</td>';
-                echo '<td>' . $row['emailaddress'] . '</td>';
-                echo '</tr>';
-            }
-            echo '</table>';
+        // ユーザー情報を取得
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // パスワードの検証
+        if ($user && $user_input_password === $user['password']) {
+            // ログイン成功時にセッションにユーザー名を保存
+            $_SESSION['username'] = $user['username'];
+
+            // ログイン成功時にsearch.htmlにリダイレクト
+            header('Location: ../search/search.html');
+            exit;
         } else {
-            echo 'データがありません';
+            echo 'メールアドレスまたはパスワードが正しくありません。';
         }
-
-
-    } catch (PDOException $e) {
-        echo '接続失敗' . $e->getMessage();
-        exit();
     }
+
+    $dbh = null;
+
+} catch (PDOException $e) {
+    echo '接続失敗' . $e->getMessage();
+    exit();
+}
 ?>
